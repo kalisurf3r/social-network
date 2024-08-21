6fb75caf6f86892f4ea2e214a466c29bd0f57409
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-20 17:27:56 -0700

    fs

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction'
+        }
+    ],
+    toJSON: {
+        virtuals: true,
+    }
+});
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ]
+    });
+
+const User = model('User', UserSchema);
+
+module.exports = User;
